4303e93921f6f2800fe0471a69d1ff50311d274f
Caio Vitor <caio199307@gmail.com>
2021-06-09 03:01:17 -0300

    Implementação do put para usuário, possibilitando alteração de dados.

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -34,6 +34,7 @@ export class InicioComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    window.scroll(0,0)
     if(environment.token == ""){
       console.log(this.router)
       console.log
